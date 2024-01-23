6221cc245a4edca989a619a69f9db5c13ad17220
tbykdr <toby.kadri@warwick.ac.uk>
2024-01-23 12:08:03 +0000

    Update bracelets.py

--- Week 2/bracelets.py
+++ Week 2/bracelets.py
@@ -1,4 +1,6 @@
-starts = [[i,j] for i in range(10) for j in range(10)]  # List of all possible starting pairs
+modulo = 10
+
+starts = [[i,j] for i in range(modulo) for j in range(modulo)]  # List of all possible starting pairs
 Bracelets = []  # Holder for all bracelets
 
 while starts:   # Runs until all possible starting pairs have been depleted
@@ -6,11 +8,11 @@ while starts:   # Runs until all possible starting pairs have been depleted
     pair = starts[0]    # Gets new unused starting pair
     starts.remove(pair)
     bracelet.append(pair)   # Transfers pair to new bracelet
-    current = [pair[1], (pair[0]+pair[1])%10]
+    current = [pair[1], (pair[0]+pair[1])%modulo]
     while current != pair:  # Iterates until back at the starting pair
         starts.remove(current)
         bracelet.append(current)
-        current = [current[1], (current[0]+current[1])%10]
+        current = [current[1], (current[0]+current[1])%modulo]
     Bracelets.append(bracelet)  # Adds new bracelet to list
 
 for bracelet in Bracelets:  # Prints all bracelets and lengths
